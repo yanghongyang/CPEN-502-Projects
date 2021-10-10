76a2c30ab1db1c7d2d1a1871cb276ff0a71cfbf6
Hongyang Yang <15652575067@163.com>
2021-10-10 11:18:06 -0700

    Complete the save error function and the framework of main function(probably)

--- Assignment-1/src/Main.java
+++ Assignment-1/src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
+        NeuralNet xor = new NeuralNet();
+
+        xor.initializeTrainSet();
+
+
+        // Train for 3000 times(Binary representation)
+        int times = 3000;
+        int epoch = 0;
+        for(int i = 0; i < times; i++) {
+            xor.initializeWeights();
+            epoch += xor.trainNet();
+        }
+        epoch /= 100;
+        System.out.println("Average epoch: " + epoch);
+        xor.saveError();
 
     }
 }
--- Assignment-1/src/NeuralNet.java
+++ Assignment-1/src/NeuralNet.java
@@ -239,4 +239,5 @@ public class NeuralNet implements NeuralNetInterface{
             e.printStackTrace();
         }
     }
+
 }
