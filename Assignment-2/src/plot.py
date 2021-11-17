570869c4d153a4e054d5a1b2d3ee5bb8ce4ea20e
Hongyang Yang <15652575067@163.com>
2021-11-17 09:37:23 -0800

    add plot script

---
+++ Assignment-2/src/plot.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    filename = "E:\\UBC_Graduate\\Term_1\\CPEN%20502\\作业\\Code\\CPEN-502-Projects\\Assignment-2\\out\\production\\Assignment-2\\RLRobot\\IRobot.data\\IRobot-winningRate.log"
+    round = []
+    winningrate = []
+    f = open(filename)
+    line = f.readline()
+    while line:
+        round.append(float(line.split(' ')[0]))
+        winningrate.append(float(line.split(' ')[1].replace('\n', '').replace('\r', '')))
+        line = f.readline()
+    f.close()
+    print(round)
+    print(winningrate)
+    plt.plot(round, winningrate)
+    plt.xlabel('# of 20 rounds')
+    plt.ylabel('Winning rate')
+    plt.title('2-a)')
+    plt.show()
+
+
+
